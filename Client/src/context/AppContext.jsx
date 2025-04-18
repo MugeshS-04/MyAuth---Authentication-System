da38a4af216074a6c27f0985917923df73e2de9c
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-04-18 15:53:41 +0530

    Fixed the refresh login status bug

--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { toast } from 'react-toastify'
 import axios from "axios";
 import { Navigate } from "react-router-dom";
@@ -6,9 +6,25 @@ import { Navigate } from "react-router-dom";
 export const AppContext = createContext();
 
 export const AppProvider = (props) => {
+        axios.defaults.withCredentials = true;
+
         const backendUrl = import.meta.env.VITE_BACKEND_URL;
         const [isLoggedIn, setIsLoggedIn] = useState(false);
         const [userData, setUserData] = useState(false);
+
+        const getauthstatus = async () => {
+                try {
+                        const {data} = await axios.get(backendUrl + 'api/auth/is-auth')
+                        if(data.success)
+                        {
+                                setIsLoggedIn(true);
+                                getUserData()
+                        }
+                } catch (error) {
+                        
+                        toast.dark("Error")
+                }
+        }
 
         const getUserData = async () => {
                 try {
@@ -16,6 +32,7 @@ export const AppProvider = (props) => {
                         if(data.success)
                         {
                                 setUserData(data.userData)
+                                setIsLoggedIn(true);
                         }
                         else{
                                 toast.error("Error Occured!"+ data.message)
@@ -25,6 +42,11 @@ export const AppProvider = (props) => {
                         toast.error("Error Occured!")
                 }
         }
+
+
+        useEffect(() => {
+                getauthstatus()
+        },[])
 
         const value = {
             backendUrl,
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -9,7 +9,7 @@ authRouter.post('/login', login)
 authRouter.post('/logout', logout)
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOTP)
 authRouter.post('/verify-account',userAuth, verifyEmail)
-authRouter.post('/is-auth',userAuth, isAuthenticated)
+authRouter.get('/is-auth',userAuth, isAuthenticated)
 authRouter.post('/send-reset-otp', sentresetotp)
 authRouter.post('/reset-password', resetpassword)
 
