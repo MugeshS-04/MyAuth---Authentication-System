53f6324ab7dd294c03e1f2273e6c6819cfa6d87d
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-04-19 22:06:10 +0530

    Update server.js
    Added the frontend url

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://my-auth-authentication-system.vercel.app/']
 
 app.use(express.json());
 app.use(cookieParser());
